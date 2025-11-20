f699973f1f0e3c3ed038015ffefb14904fcf093b
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-20 23:03:31 +0300

    print_int.c

--- print_int.c
+++ print_int.c
@@ -1,32 +1,57 @@
 #include "main.h"
 
 /**
- * print_int - prints a signed integer
- * @args: va_list containing the integer
+ * print_number - prints a positive number (recursive)
+ * @n: number to print (>= 0)
  *
  * Return: number of characters printed, or -1 on error
  */
-int print_int(va_list args)
+static int print_number(unsigned long n)
 {
-    int n = va_arg(args, int);
-    unsigned int num;
-    int count = 0, printed;
+    int count = 0;
+    int tmp;
 
-    if (n < 0)
+    if (n / 10)
+    {
+        tmp = print_number(n / 10);
+        if (tmp == -1)
+            return (-1);
+        count += tmp;
+    }
+
+    if (_putchar((n % 10) + '0') == -1)
+        return (-1);
+
+    return (count + 1);
+}
+
+/**
+ * print_int - prints a signed integer (%d and %i)
+ * @ap: argument list
+ *
+ * Return: number of characters printed, or -1 on error
+ */
+int print_int(va_list ap)
+{
+    int n = va_arg(ap, int);
+    long ln = n;
+    unsigned long num;
+    int count = 0;
+    int tmp;
+
+    if (ln < 0)
     {
         if (_putchar('-') == -1)
             return (-1);
-        num = (unsigned int)(-n);
         count++;
-    }
-    else
-    {
-        num = (unsigned int)n;
+        ln = -ln;
     }
 
-    printed = print_unsigned(num);
-    if (printed == -1)
+    num = (unsigned long)ln;
+
+    tmp = print_number(num);
+    if (tmp == -1)
         return (-1);
 
-    return (count + printed);
+    return (count + tmp);
 }
