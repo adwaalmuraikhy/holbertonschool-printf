144f9d8a25b23a0e3b7579512eda553e0b1cbd2e
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-11-21 02:30:12 +0300

    print_string.c

--- print_string.c
+++ print_string.c
@@ -8,18 +8,18 @@
  */
 int print_string(va_list ap)
 {
-    char *s = va_arg(ap, char *);
-    int count = 0;
+	char *s = va_arg(ap, char *);
+	int count = 0;
 
-    if (s == NULL)
-        s = "(null)";
+	if (s == NULL)
+		s = "(null)";
 
-    while (s[count] != '\0')
-    {
-        if (_putchar(s[count]) == -1)
-            return (-1);
-        count++;
-    }
+	while (s[count] != '\0')
+	{
+		if (_putchar(s[count]) == -1)
+			return (-1);
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
